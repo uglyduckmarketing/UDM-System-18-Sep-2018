76397d9ba537188956fca3a6c91816fca9ef16e1
HarmanUDM <onlinequeries@ymail.com>
2018-12-26 10:34:10 +0530

    basic header

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">				
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="https://fonts.googleapis.com/css?family=Barlow:400,500,600" rel="stylesheet">	  			
 			
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/magnific-popup.css">				
 <?php wp_head(); ?>
